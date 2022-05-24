744ed4c32ec5a3b170d3bf74575dd64860ecb26e
adam <adam.bunce11@gmail.com>
2022-05-24 01:17:12 -0400

    removed exception

--- src/main/java/com/apm/ApmKeyListener.java
+++ src/main/java/com/apm/ApmKeyListener.java
@@ -2,13 +2,12 @@ package com.apm;
 
 import net.runelite.client.input.KeyListener;
 import java.awt.event.KeyEvent;
-import java.io.FileNotFoundException;
 
 public class ApmKeyListener implements KeyListener {
     public final ApmPlugin plugin;
     public final ApmConfig config;
 
-    ApmKeyListener(ApmPlugin plugin, ApmConfig config) throws FileNotFoundException {
+    ApmKeyListener(ApmPlugin plugin, ApmConfig config) {
         this.plugin = plugin;
         this.config = config;
     }
